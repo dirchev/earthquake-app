077454a2cd13fdcc8fb71091bd10d6362a5e0890
Dimitar Mirchev <dirchev@camplight.net>
2019-02-14 08:52:08 +0000

    add file header template for Java classes

---
+++ .idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,9 @@
+/**
+  *
+  * Mobile Platform Development Coursework 2019
+  *   Name:                    Dimitar Mirchev
+  *   Student ID:              S1515512
+  *   Programme of study:      Computing
+  * ${YEAR} ${MONTH_NAME_FULL} ${DAY}
+  */
+ 
