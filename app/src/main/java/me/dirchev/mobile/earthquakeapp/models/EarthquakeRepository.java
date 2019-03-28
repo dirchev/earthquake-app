5396894ee0da2679c5d50edd04974d56058740e7
Dimitar Mirchev <dirchev@camplight.net>
2019-03-28 10:45:31 +0000

    Update EarthquakeRepository filters handling
    1. Add better processing of filters
    2. Add helper method to display filters information
    3. Add helper method to display results information

--- app/src/main/java/me/dirchev/mobile/earthquakeapp/models/EarthquakeRepository.java
+++ app/src/main/java/me/dirchev/mobile/earthquakeapp/models/EarthquakeRepository.java
@@ -2,6 +2,8 @@ package me.dirchev.mobile.earthquakeapp.models;
 
 import android.util.Log;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -18,11 +20,12 @@ import java.util.Observable;
  */
 public class EarthquakeRepository {
     List<Earthquake> earthquakeList;
+    List<Earthquake> filteredEarthquakes;
     Map<String, List<Earthquake>> earthquakesByDateList;
+    Map<String, Object> filters;
 
     List<EarthquakeRepositoryChangeListener> changeListeners;
     int selectedEarthquakeIndex = -1;
-    Date selectedDate = new Date();
 
     public static String getStringFromEarthquakePubDate (Date date) {
         return date.getDate() + "/" + date.getMonth() + "/" + date.getYear();
@@ -32,7 +35,30 @@ public class EarthquakeRepository {
         this.earthquakeList = new LinkedList<>();
         this.changeListeners = new LinkedList<>();
         this.earthquakesByDateList = new HashMap<>();
-        Log.e("MyTag/initial date", this.getStringFromEarthquakePubDate(this.selectedDate));
+        this.filters = new HashMap<>();
+    }
+
+    public void setFilter(String filterName, Object filterValue) {
+        if (filterValue != null) {
+            this.filters.put(filterName, filterValue);
+        } else {
+            this.filters.remove(filterName);
+        }
+        processFilteredEarthquakes();
+        updateAllListeners();
+    }
+
+    private void processFilteredEarthquakes () {
+        Date startDateFilter = (Date) filters.get("startDate");
+        Date endDateFilter = (Date) filters.get("endDate");
+        String textFilter = (String) filters.get("text");
+        this.filteredEarthquakes = new LinkedList<>();
+        for (Earthquake current : earthquakeList) {
+            if (startDateFilter != null && current.getPubDate().before(startDateFilter)) continue;
+            if (endDateFilter != null && current.getPubDate().after(endDateFilter)) continue;
+            if (textFilter != null && !current.getLocationName().contains(textFilter)) continue;
+            this.filteredEarthquakes.add(current);
+        }
     }
 
     public void addEarthquake (Earthquake earthquake) {
@@ -46,41 +72,17 @@ public class EarthquakeRepository {
             earthquakesByDateList.put(dateString, newListForDate);
             Log.e("MyTag/ new date", dateString);
         }
-    }
-
-    public Earthquake getEarthquakeByIndex (int index) {
-        return earthquakeList.get(index);
-    }
-
-    public int size () {
-        return earthquakeList.size();
+        processFilteredEarthquakes();
     }
 
     public Earthquake getSelectedEarthquake () {
         if (this.selectedEarthquakeIndex == -1) return null;
-        return this.getEarthquakesForDate().get(this.selectedEarthquakeIndex);
+        return this.getVisibleEarthquakes().get(this.selectedEarthquakeIndex);
     }
 
-    public List<Earthquake> getEarthquakesForDate () {
-        List<Earthquake> list = earthquakesByDateList.get(this.getStringFromEarthquakePubDate(this.selectedDate));
-        if (list == null) {
-            return new LinkedList<>();
-        }
-        return list;
-    }
 
     public void setSelectedEarthquakeIndex (int index) {
         this.selectedEarthquakeIndex = index;
-        this.updateAllListeners();
-    }
-
-    public Date getSelectedDate() {
-        return selectedDate;
-    }
-
-    public void setSelectedDate(Date selectedDate) {
-        this.selectedDate = selectedDate;
-        this.selectedEarthquakeIndex = -1;
         this.updateAllListeners();
     }
 
@@ -90,12 +92,54 @@ public class EarthquakeRepository {
         }
     }
 
+    public List<Earthquake> getVisibleEarthquakes () {
+        return filteredEarthquakes;
+    }
+
     public void subscribeToChange (EarthquakeRepositoryChangeListener listener) {
         this.changeListeners.add(listener);
     }
 
     public void unsubscribeToChange (EarthquakeRepositoryChangeListener listener) {
         this.changeListeners.remove(listener);
+    }
+
+    public String getFiltersInfoString () {
+        LinkedList<String> resultList = new LinkedList<>();
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
+        if (this.filters.containsKey("text") && !((String) this.filters.get("text")).isEmpty()) {
+            resultList.add("\"" + this.filters.get("text") + "\"");
+        }
+        if (this.filters.containsKey("startDate")) {
+            resultList.add(dateFormat.format((Date) this.filters.get("startDate")));
+        } else {
+            resultList.add("beginning");
+        }
+
+        if (this.filters.containsKey("endDate")) {
+            resultList.add(dateFormat.format((Date) this.filters.get("endDate")));
+        } else {
+            resultList.add("today");
+        }
+
+        String result = "";
+        for (String item : resultList) {
+            if (result.isEmpty()) {
+                result += item;
+            } else {
+                result += " - " + item;
+            }
+        }
+        return result;
+    }
+
+    public String gerResultsInfoString () {
+        int numberOfItems = this.getVisibleEarthquakes().size();
+        if (numberOfItems == 0) {
+            return "No earthquakes found for these filters";
+        } else {
+            return "Showing " + numberOfItems + " earthquakes.";
+        }
     }
 
     @Override
