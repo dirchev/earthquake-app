300f5401ea29edf7f34e95bff1e018ae8316096e
Dimitar Mirchev <dirchev@camplight.net>
2019-03-28 10:46:59 +0000

    finish implementation of filters

--- app/src/main/java/me/dirchev/mobile/earthquakeapp/MainActivity.java
+++ app/src/main/java/me/dirchev/mobile/earthquakeapp/MainActivity.java
@@ -1,24 +1,39 @@
 package me.dirchev.mobile.earthquakeapp;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.net.ParseException;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -39,10 +54,47 @@ import me.dirchev.mobile.earthquakeapp.models.EarthquakeRepositoryChangeListener
  */
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 {
+    private DatePickerDialog datePickerDialog;
+    class DateInputOnFonusChangeListener implements View.OnFocusChangeListener {
+        public void onFocusChange(final View v, boolean hasFocus) {
+            final EditText currentDateInput = (EditText) v;
+            int mYear, mMonth, mDay;
+
+            if (hasFocus) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+                datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                currentDateInput.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            } else {
+                datePickerDialog.hide();
+                datePickerDialog = null;
+            }
+        }
+    }
+    private ViewGroup filtersButtonsContainer;
     private RecyclerView recyclerView;
-    private Button nextDayButton;
-    private Button prevDayButton;
-    private TextView dateTextView;
+    private TextView filtersInfo;
+    private TextView resultsInfo;
+    private EditText startDateInput;
+    private EditText endDateInput;
+    private EditText searchInput;
+    private Button toggleFiltersButton;
+    private Button closeFiltersButton;
+    private Button clearFiltersButton;
+    private Button updateFiltersButton;
+    private SupportMapFragment mapFragment;
+    private ViewGroup additionalFilters;
     private String urlSource="http://quakes.bgs.ac.uk/feeds/MhSeismology.xml";
     EarthquakeDataStore dataStore = EarthquakeDataStore.getInstance();
 
@@ -53,27 +105,106 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
         startProgress();
         recyclerView = (RecyclerView) findViewById(R.id.earthquake_list_recycler_view);
-        nextDayButton = findViewById(R.id.nextDay);
-        prevDayButton = findViewById(R.id.prevDay);
-        dateTextView = findViewById(R.id.button1);
-        prevDayButton.setOnClickListener(new OnClickListener() {
+        filtersInfo = findViewById(R.id.filtersInfo);
+        resultsInfo = findViewById(R.id.resultsInfo);
+        additionalFilters = findViewById(R.id.additionalFilters);
+        startDateInput = findViewById(R.id.startDateInput);
+        endDateInput = findViewById(R.id.endDateInput);
+        searchInput = findViewById(R.id.searchInput);
+        DateInputOnFonusChangeListener dateInputOnFonusChangeListener = new DateInputOnFonusChangeListener();
+        startDateInput.setOnFocusChangeListener(dateInputOnFonusChangeListener);
+        endDateInput.setOnFocusChangeListener(dateInputOnFonusChangeListener);
+        toggleFiltersButton = findViewById(R.id.toggleFiltersButton);
+        closeFiltersButton = findViewById(R.id.closeFiltersButton);
+        clearFiltersButton = findViewById(R.id.clearFiltersButton);
+        updateFiltersButton = findViewById(R.id.updateFiltersButton);
+        filtersButtonsContainer = findViewById(R.id.filtersButtonsContainer);
+
+        closeFiltersButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Date newDate = (Date) dataStore.getEarthquakeRepository().getSelectedDate().clone();
-                newDate.setDate(newDate.getDate() - 1);
-                dataStore.getEarthquakeRepository().setSelectedDate(newDate);
-                dateTextView.setText(EarthquakeRepository.getStringFromEarthquakePubDate(newDate));
+                updateFiltersVisibility(false);
             }
         });
-        nextDayButton.setOnClickListener(new OnClickListener() {
+        clearFiltersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Date newDate = (Date) dataStore.getEarthquakeRepository().getSelectedDate().clone();
-                newDate.setDate(newDate.getDate() + 1);
-                dataStore.getEarthquakeRepository().setSelectedDate(newDate);
-                dateTextView.setText(EarthquakeRepository.getStringFromEarthquakePubDate(newDate));
+                clearFilters();
             }
         });
+        updateFiltersButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processFilters();
+                updateFiltersVisibility(false);
+            }
+        });
+        toggleFiltersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateFiltersVisibility(true);
+            }
+        });
+    }
+
+    private Date stringToDate (String dateString) {
+        if (dateString.isEmpty()) return null;
+        String[] dateStringParts = dateString.split("-");
+        if (dateStringParts.length != 3) return null;
+        Calendar cal = Calendar.getInstance();
+        try {
+            cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(dateStringParts[0]));
+            cal.set(Calendar.MONTH, Integer.parseInt(dateStringParts[1]) - 1);
+            cal.set(Calendar.YEAR, Integer.parseInt(dateStringParts[2]));
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+        } catch (ParseException e) {
+            return null;
+        }
+
+        return cal.getTime();
+    }
+
+    private void clearFilters () {
+        startDateInput.setText(null);
+        endDateInput.setText(null);
+        searchInput.setText(null);
+    }
+
+    private void processFilters () {
+        Date startDate = startDateInput.getText() == null
+              ? null
+              : stringToDate(startDateInput.getText().toString());
+        Date endDate = endDateInput.getText() == null
+              ? null
+              : stringToDate(endDateInput.getText().toString());
+        String searchTerm = searchInput.getText() == null
+              ? null
+              : searchInput.getText().toString();
+        EarthquakeRepository earthquakeRepository = dataStore.getEarthquakeRepository();
+        earthquakeRepository.setFilter("startDate", startDate);
+        earthquakeRepository.setFilter("endDate", endDate);
+        earthquakeRepository.setFilter("text", searchTerm);
+    }
+
+    private void updateFiltersVisibility (boolean isVisible) {
+        if (isVisible) {
+            toggleFiltersButton.setVisibility(View.GONE);
+            mapFragment.getView().setVisibility(View.GONE);
+            additionalFilters.setVisibility(View.VISIBLE);
+            filtersInfo.setVisibility(View.GONE);
+            searchInput.setVisibility(View.VISIBLE);
+            filtersButtonsContainer.setVisibility(View.VISIBLE);
+        } else {
+            toggleFiltersButton.setVisibility(View.VISIBLE);
+            mapFragment.getView().setVisibility(View.VISIBLE);
+            additionalFilters.setVisibility(View.GONE);
+            filtersInfo.setVisibility(View.VISIBLE);
+            searchInput.setVisibility(View.GONE);
+            filtersButtonsContainer.setVisibility(View.GONE);
+        }
     }
 
     public void startProgress()
@@ -91,11 +222,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
                             @Override
                             public void onChange(EarthquakeRepository earthquakeRepository) {
                                 radapter.notifyDataSetChanged();
+                                filtersInfo.setText(earthquakeRepository.getFiltersInfoString());
+                                resultsInfo.setText(earthquakeRepository.gerResultsInfoString());
+
                             }
                         });
                         recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
                         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
                         mapFragment.getMapAsync(MainActivity.this);
                     }
                 });
@@ -130,7 +264,15 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.clear();
         if (earthquake == null) return;
         LatLng place = earthquake.getLocation();
-        googleMap.addMarker(new MarkerOptions().position(place).title("Here it is"));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLng(earthquake.getLocation()));
+//        googleMap.addMarker(new MarkerOptions().position(place).title("Here it is"));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(earthquake.getLocation())
+                .zoom(10)
+                .build();
+        CircleOptions circleOptions = new CircleOptions()
+                .center(earthquake.getLocation())
+                .radius(Math.abs(earthquake.getMagnitude()) * 1000);
+        Circle circle = googleMap.addCircle(circleOptions);
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
