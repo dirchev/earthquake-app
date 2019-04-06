337e55fe677b55c45f0b3dd8ec6fd8e50c001861
Dimitar Mirchev <dirchev@camplight.net>
2019-04-06 19:29:23 +0100

    remove empty line

--- app/src/main/java/me/dirchev/mobile/earthquakeapp/data/EarthquakeParser.java
+++ app/src/main/java/me/dirchev/mobile/earthquakeapp/data/EarthquakeParser.java
@@ -65,7 +65,6 @@ public class EarthquakeParser {
                     }
                 }
                 eventType = xpp.next();
-
             }
         }
         catch (XmlPullParserException ae1)
